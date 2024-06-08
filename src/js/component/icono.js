5a6b71b50b9eed76f55a1d02f466c3c65cb07e73
Jhonny Timaure <164609178+TIMAURE@users.noreply.github.com>
2024-06-08 11:28:55 +0000

    primerCommit

---
+++ src/js/component/icono.js
@@ -0,0 +1,10 @@
+//1) First you have to import the @fortawesome/fontawesome library
+//
+import fontawesome from '@fortawesome/fontawesome';
+import faClock from '@fortawesome/fontawesome-free-regular/faClock';
+
+fontawesome.config = {
+  autoReplaceSvg: 'nest'
+};
+fontawesome.library.add(faClock);
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,44 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import propsTypes from 'prop-types';
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+function ContadorSimple(props){
+    return (
+        <div className="Externo">
+             <div className="Reloj">
+                <i class="far fa-clock">
+             </i></div>
+             <div className="Cuarto">{props.digitoCuatro % 10}</div>
+             <div className="Tercer">{props.digitoTres % 10}</div>
+             <div className="Segundo">{props.digitoDos % 10}</div>
+            <div className="Primero">{props.digitoUno % 10}</div>
+        </div>
+    );
+};
+ContadorSimple.propsTypes ={
+    digitoCuatro : propsTypes.number,
+    digitoTres : propsTypes.number,
+    digitoDos : propsTypes.number,
+    digitoUno: propsTypes.number,
+};
+let counter = 0;
+setInterval(function(){
+    const Cuarto = Math.floor(counter/1000);
+    const Tercer = Math.floor(counter/100);
+    const Segundo = Math.floor(counter/10);
+    const Primero = Math.floor(counter/1);
+    counter ++;
+
+    ReactDOM.render(<ContadorSimple digitoUno={Primero} digitoDos={Segundo} digitoTres={Tercer} digitoCuatro={Cuarto} />, document.querySelector("#app"));
+},1000)
+
+
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
